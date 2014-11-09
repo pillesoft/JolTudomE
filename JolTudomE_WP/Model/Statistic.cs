d17c7bdc220ed8d273e297bb41508ddfc39032c7
pillesoft <pillesoft@gmail.com>
2014-11-09 19:08:01 +0100

    architektura, hasznalati eset diagramok statisztika oldal formazasa

--- JolTudomE_WP/Model/Statistic.cs
+++ JolTudomE_WP/Model/Statistic.cs
@@ -1,39 +1,14 @@
 ﻿using System;
 
 namespace JolTudomE_WP.Model {
-  public class Statistic : BaseNotifyable {
+  public class Statistic {
 
-    private int _CorrectAnswer;
-    private DateTime _Generated;
-    private Nullable<decimal> _Percent;
-    private Nullable<int> _Questions;
-    private int _TestID;
-    private Nullable<TimeSpan> _TotalTime;
-
-    public int CorrectAnswer {
-      get { return this._CorrectAnswer; }
-      set { _CorrectAnswer = value; }
-    }
-    public DateTime Generated {
-      get { return this._Generated; }
-      set { _Generated = value; }
-    }
-    public Nullable<decimal> Percent {
-      get { return _Percent; }
-      set { _Percent = value; }
-    }
-    public Nullable<int> Questions {
-      get { return _Questions; }
-      set { _Questions = value; }
-    }
-    public int TestID {
-      get { return _TestID; }
-      set { _TestID = value; }
-    }
-    public Nullable<TimeSpan> TotalTime {
-      get { return _TotalTime; }
-      set { _TotalTime = value; }
-    }
+    public int CorrectAnswer { get;set; }
+    public DateTime Generated { get; set; }
+    public Nullable<decimal> Percent { get; set; }
+    public Nullable<int> Questions { get; set; }
+    public int TestID { get; set; }
+    public Nullable<TimeSpan> TotalTime { get; set; }
 
     public string NumberOfQuestAnsw {
       get { return string.Format("{0}/{1}", CorrectAnswer, Questions); }
@@ -41,6 +16,10 @@ namespace JolTudomE_WP.Model {
 
     public string PercentString {
       get { return string.Format("{0:P}", Percent); }
+    }
+
+    public string GeneratedString {
+      get { return Generated.ToString("g"); }
     }
   }
 
